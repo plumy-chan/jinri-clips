fac4817febcfa23b0b6bf8febe60adb14ac4c301
plumy-chan <92406075+plumy-chan@users.noreply.github.com>
2022-01-31 01:21:00 -0800

    Added file via upload.
    Initial file

---
+++ list.js
@@ -0,0 +1,23 @@
+// One Google Drive file ID per line
+let clipIDs = ["1mugx5YInczmvJoZ3xcsXmpD__wisrVYB", //vod-1157591777-offset-3460.mp4
+               "1pPrJMmn6tCQildhUHJrowEXEOPkakRw_", //AT-cm_z3Qu89VoaoszWIThSrbCsQ.mp4
+               "1-RB0iPxKZoZoWmPy2vzIGtHSXat8T4xp", //AT-cm_yfc0o1dw64uWd7I1j5CIsQ (1).mp4
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+               "", //
+''];
